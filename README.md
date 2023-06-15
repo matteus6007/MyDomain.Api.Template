13234d5be359ecc5d99bbcb4315399511a7948a7
Matt Jones <matthew_a_jones@hotmail.com>
2023-06-15 15:47:59 +0100

    Add Serilog for structured logging

--- README.md
+++ README.md
@@ -8,3 +8,27 @@ Performed on `pre-commit` using [Husky.Net](https://alirezanet.github.io/Husky.N
 The linting of the codebase is done using `dotnet format` which is included within the dotnet SDK, from version `6.0`.
 
 _Note: for new repositories, as dotnet format will only run against changed files, an 'Initial commit' needs to be present to allow Husky to determine which files are staged._
+
+## Logging
+
+Structured logging using [Serilog](https://serilog.net/).
+
+Example configuration in `appsettings.json`:
+
+```json
+"Serilog": {
+    "Using": ["Serilog.Sinks.Console"],
+    "MinimumLevel": {
+        "Default": "Information",
+        "Override": {
+            "Microsoft": "Warning",
+            "Microsoft.AspNetCore": "Error",
+            "System": "Warning"
+        }
+    },
+    "WriteTo": [
+        { "Name": "Console" }
+    ],
+    "Enrich": ["FromLogContext"]
+}
+```
--- src/api/MyDomain.Api/Program.cs
+++ src/api/MyDomain.Api/Program.cs
@@ -1,16 +1,19 @@
 using MyDomain.Application;
 using MyDomain.Infrastructure;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
     builder.Services.AddApplication();
     builder.Services.AddInfrastructure();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 }
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog((ctx, cfg) => cfg.ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 
@@ -21,10 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
