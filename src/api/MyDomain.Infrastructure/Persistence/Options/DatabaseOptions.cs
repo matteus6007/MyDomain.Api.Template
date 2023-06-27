7005e58ccefbb90a562033c9e6c97d339c668215
Matt Jones <matthew_a_jones@hotmail.com>
2023-06-27 13:46:53 +0100

    Fix DatabaseOptions warnings

--- src/api/MyDomain.Infrastructure/Persistence/Options/DatabaseOptions.cs
+++ src/api/MyDomain.Infrastructure/Persistence/Options/DatabaseOptions.cs
@@ -2,10 +2,10 @@ namespace MyDomain.Infrastructure.Persistence.Options;
 
 public class DatabaseOptions
 {
-    public string Server { get; set; }
-    public string ReplicaServer { get; set; }
+    public required string Server { get; set; }
+    public string ReplicaServer { get; set; } = "";
     public int? Port { get; set; }
-    public string Schema { get; set; }
-    public string User { get; set; }
-    public string Password { get; set; }
+    public required string Schema { get; set; }
+    public required string User { get; set; }
+    public required string Password { get; set; }
 }
