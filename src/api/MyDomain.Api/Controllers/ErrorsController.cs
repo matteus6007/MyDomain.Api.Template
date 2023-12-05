d74cbb13b19f63e6a942e29c11fcddf71fa43955
Matt Jones <matthew_a_jones@hotmail.com>
2023-12-05 11:18:45 +0000

    Fix Swagger error

--- src/api/MyDomain.Api/Controllers/ErrorsController.cs
+++ src/api/MyDomain.Api/Controllers/ErrorsController.cs
@@ -1,11 +1,21 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyDomain.Api.Controllers;
 
+/// <summary>
+/// Handle unexpected errors
+/// </summary>
 public class ErrorsController : ControllerBase
 {
+    /// <summary>
+    /// Display API errors
+    /// </summary>
+    /// <returns>Error message</returns>
     [Route("/error")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [AllowAnonymous]
     public IActionResult Error()
     {
         Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
